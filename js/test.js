0dd4377178b1dc50d478c2447148836e492ed43f
nibibot <luke.yoon18@gmail.com>
2016-10-22 15:34:06 -0400

    f3

--- js/test.js
+++ js/test.js
@@ -115,7 +115,7 @@ var gameArea = {
 		
 		//Add platform
 		if (currTime - prevTime > 500) {
-			var notesArr = ['A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5'];
+			var notesArr = ['A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4'];
 			var nt = notesArr[parseInt(Math.random()*7)];
 			platforms.push(new generatePlatform(true, gamePiece.x+Math.random()*100+150, Math.random()*50+300, 100, 30, nt));
 			prevTime = currTime;
