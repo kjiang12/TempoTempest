7d07053c9ec4312837a450f552ce7180c017e59b
nibibot <luke.yoon18@gmail.com>
2016-10-21 23:31:37 -0400

    added js

---
+++ js/test.js
@@ -0,0 +1,61 @@
+var myGamePiece;
+
+function startGame() {
+	myGamePiece = new component(30, 30, "red", 10, 120);
+	myGameArea.start();
+}
+
+var myGameArea = {
+	canvas : document.createElement("canvas"),
+	start : function() {
+		this.canvas.width = 1000;
+		this.canvas.height = 720;
+		this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea, 1);
+	    window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = true;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = false; 
+        })
+	},
+	clear : function(){
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y; 
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY; 
+    } 
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0; 
+    if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -5; }
+    if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 5; }
+    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -5; }
+    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 5; }
+    myGamePiece.newPos(); 
+    myGamePiece.update();
+}
+
+function generatePlatform(x, y) {
+	
+}
